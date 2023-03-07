db7d20525a2688514c8598d1b2e940e014c8d835
LaUrrego <lurrego22@gmail.com>
2023-03-06 23:47:49 -0800

    Change to portfolio project

--- CheckersGame.py
+++ CheckersGame.py
@@ -21,17 +21,82 @@ class InvalidPlayer(Exception):
     pass
 
 
-class CheckerBoard:
+class Token:
+    """Represents a checkers piece token. Can take the form of a regular piece, King or Triple King. Includes
+    methods to move it based on official rules"""
+    def __init__(self, position, color):
+        # can be "Regular", "King" or "TripleKing"
+        self._type = "Regular"
+        self._current_position = position
+        self._color = color
 
+    def move(self, start_position, end_position, game_board):
+        if self._type == "Regular":
+            pass
+
+    def get_position(self):
+        """Return where this token is current placed"""
+        return self._current_position
+
+    def get_possible_moves(self, game_board):
+        """Returns possible moves for this token"""
+        row, column = self._current_position
+        possible_moves = []
+        if self._type == "Regular":
+            # black piece logic
+            if self._color == "Black":
+                if row == 0:
+                    print("TOP EDGE, CHANGE TO KING")
+                    return
+                row_above = game_board[row - 1]
+                # Search for potential adjacent enemy piece
+                if "White" in row_above:
+                    adjacent_candidate = []
+                    for index, square in enumerate(game_board[row - 1]):
+                        # white piece adjacent
+                        if square == "White" and (index == column + 1 or index == column - 1) and row > 1:
+                            # add candidates
+                            adjacent_candidate.append((row-1, index))
+                    print("adjacent: ", adjacent_candidate)
+                    two_above = game_board[row - 2]
+                    for candidates in adjacent_candidate:
+                        x_pos, y_pos = candidates
+                        # on the right
+                        if y_pos > column:
+                            if two_above[y_pos + 1] == "OK":
+                                possible_moves.append((row-2, y_pos + 1))
+                        # on the left
+                        elif y_pos < column:
+                            if two_above[y_pos - 1] == "OK":
+                                possible_moves.append((row-2, y_pos - 1))
+                    return possible_moves
+                # No white in row above piece
+                else:
+                    for index, square in enumerate(game_board[row - 1]):
+                        if square == "OK" and (index == column + 1 or index == column - 1):
+                            # add empty space not occupied by enemy or None
+                            possible_moves.append((row - 1, index))
+                return possible_moves
+
+            # white piece logic
+            elif self._color == "White":
+                pass
+
+
+
+
+
+class CheckerBoard:
+    """Class simulating a checkerboard object. Has methods to create a board as well as fill it with white and black
+    tokens to begin gameplay"""
     def __init__(self):
         # initiate board with "OK" for valid black squares and None for invalid white squares
         new_board = self.filler("OK", None)
         # board data member
         self._board = new_board
-        # Sets up board with player pieces
-        self.start_setup()
 
     def get_board(self):
+        """Get method to return the current board"""
         return self._board
 
     def filler(self, color1, color2):
@@ -59,16 +124,20 @@ class CheckerBoard:
     def start_setup(self):
         """Sets up the board with top three rows White, bottom 3 rows Black"""
         # set White
+        white_tokens = []
         for row in range(3):
             for column in range(8):
                 if self._board[row][column] == "OK":
                     self._board[row][column] = "White"
+                    white_tokens.append(Token((row, column), "White"))
         # set Black
+        black_tokens = []
         for row in range(3):
             for column in range (8):
                 if self._board[row + 5][column] == "OK":
                     self._board[row + 5][column] = "Black"
-
+                    black_tokens.append(Token((row + 5, column), "Black"))
+        return [white_tokens, black_tokens]
 
 
 class Checkers:
@@ -80,8 +149,31 @@ class Checkers:
         # initialized as "Black" for first move
         self._current_turn = "Black"
         self._players = {}
+        self._tokens = {
+            "Black": [],
+            "White": [],
+        }
+        self._current_board = None
+        self.setup()
+
+    def test_adder(self, color, pos):
+        """tester method to add a piece in a particular place"""
+        row, column = pos
+        self._current_board[row][column] = color
+        return
+
+    def setup(self):
+        """Sets up the board with pieces and fills token collection for each player"""
         board = CheckerBoard()
+        pieces = board.start_setup()
+        # fill white pieces
+        for item in pieces[0]:
+            self._tokens["White"].append(item)
+        # fill black pieces
+        for item in pieces[1]:
+            self._tokens["Black"].append(item)
         self._current_board = board.get_board()
+
 
     def change_turn(self):
         """Function to change the color piece going next"""
@@ -91,7 +183,8 @@ class Checkers:
             self._current_turn = "Black"
         return
 
-    def valid_turn(self, player_name):
+    def valid_player(self, player_name):
+        """Checks to see if input name is the currently allowed player to move"""
         if self._players[self._current_turn] == player_name:
             return True
         return False
@@ -121,6 +214,7 @@ class Checkers:
             raise InvalidSquare("This position does not exist")
 
     def create_player(self, player_name, piece_color):
+        """Creates a player given a name and piece color. Returns the player object"""
         if piece_color.lower() == "black":
             self._players["Black"] = player_name
             return Player(player_name,"Black")
@@ -131,7 +225,8 @@ class Checkers:
             raise IncorrectColorPieceError("Piece color can only be 'White' or 'Black'!")
 
     def play_game(self, player_name, starting_square_location, destination_square_location):
-        if not self.valid_turn(player_name):
+        """Nie Wiem"""
+        if not self.valid_player(player_name):
             raise OutofTurn("It's not currently your turn!")
         else:
             # raises InvalidSquare if outside the board
@@ -145,6 +240,16 @@ class Checkers:
             # raises InvalidSquare if current player is not the owner of the starting location
             if current_player != square_owner:
                 raise InvalidSquare("This is not your piece!")
+            if player_name not in self._players.values():
+                raise InvalidPlayer("This is not a valid player!")
+            # should match one of the current pieces in play
+            for tokens in self._tokens[self._current_turn]:
+                if tokens.get_position() == starting_square_location:
+                    # test to see possible moves
+                    print(tokens.get_possible_moves(self._current_board))
+                    return
+
+
             #### continue with If the player_name is not valid, raise an InvalidPlayer exception (you'll need to define this exception class).
 
     def get_checker_details(self):
@@ -159,21 +264,26 @@ class Checkers:
 
 
 class Player:
-
+    """Simulates a player in checkers, initialized with a player_name and checker_color. Includes methods to return
+    the color, capture count, number of king pieces, and number of triple king pieces"""
     def __init__(self, player_name, checker_color):
         self._player_name = player_name
         self._checker_color = checker_color
 
     def get_checker_color(self):
+        """Returns the players checker color"""
         return self._checker_color
 
     def get_king_count(self):
+        """Returns the number of kings this player has"""
         pass
 
     def get_triple_king_count(self):
+        """Returns the number of triple kings this player has"""
         pass
 
     def get_captured_pieces_count(self):
+        """Returns the amount of captures pieces by the current player"""
         pass
 
 
@@ -182,6 +292,12 @@ game = Checkers()
 print(game.valid_square_location((0,0)))
 game.create_player("Larry", "black")
 game.create_player("Karolcia", "white")
-game.play_game("Larry", (7,0), (0,1))
+game.test_adder("White", (4,3))
+game.play_game("Larry", (5,6), (4,1))
 for row in game.print_board():
     print(row)
+
+#for color, token in game._tokens.items():
+#    print(color)
+#    for piece in token:
+#        print(piece.get_position())
